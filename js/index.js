d6cad4207b48bb673235585598fd6e48660c2ad1
Ben Koehler <bkoehler85282@gmail.com>
2019-10-31 21:03:59 -0700

    made a creepy zombie bus with event listners

--- js/index.js
+++ js/index.js
@@ -9,21 +9,15 @@ const spookyImgSrcs = [
     'https://images.unsplash.com/photo-1509557965875-b88c97052f0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
     'https://images.unsplash.com/photo-1481819434877-23b892ca68ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
 ];
-const headerTwo = document.querySelectorAll('h2');
 
-headerTwo[0].addEventListener("mouseover", item => {
-    item.target.style.color = 'blue';
-});
+const horrorWords = ['ZOMBIE BUS', 'BLOOD', 'GUTS', 'DEATH', 'GRAVE'];
 
-const scrollPage = document.querySelector('body');
+const halloweenHeadings = ['UNWELCOME TO ZOMBIE BUS', 'LET\'S DIE!', 'TORTURE AWAITS', 'PICK YOUR POISON', 'BURN IN THE SUN', 'EXCRUCIATING EXCURSION', 'ISLAND IMPALEMENT'];
 
-window.addEventListener('scroll', () => {
-    scrollPage.style.backgroundColor = "lightblue";
+const evilLaugh = 'BUAHAHAHAHAHAHAHAHAHAHAHA! AHAHAHAHAHAHAHAHA! *cough* *cough* AHAHAHAHAHAHAHA!';
 
-    setTimeout(() => {
-        scrollPage.style.backgroundColor = "";
-    }, 3000);
-});
+const zombieIpsum = 'Reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.';
+
 
 //Change images on click
 const imgs = document.querySelectorAll('img');
@@ -35,5 +29,78 @@ imgs.forEach((element, i) => {
     // Revert images on double click
     element.addEventListener('dblclick', () => {
         element.src = originalImgSrcs[i];
+    });
+});
+
+//Change background and text colors on scroll
+const body = document.querySelectorAll('.main-navigation, .nav-container a, body, footer p');
+body.forEach(element => {
+    window.addEventListener('scroll', () => {
+        element.style.color = 'orange';
+        element.style.backgroundColor = 'black';
+    });
+});
+
+//Change nav link content on hover
+const navCont = document.querySelectorAll('.logo-heading, .nav-link');
+navCont.forEach((element, i) => {
+    const originalText = element.textContent;
+    const originalColor = element.style.color;
+    element.addEventListener('mouseenter', () => {
+        element.textContent = horrorWords[i];
+        element.style.color = 'red';
+    });
+
+    element.addEventListener('mouseleave', () => {
+        element.textContent = originalText;
+        element.style.color = 'red';
+    });
+});
+
+//Change section headings on re-size
+const headings = document.querySelectorAll('h2, h3, h4');
+headings.forEach((element, i) => {
+    window.addEventListener('resize', () => {
+        element.textContent = halloweenHeadings[i];
+        element.style.color = 'red';
+    });
+});
+
+//Change all <p>'s to evilLaugh on keydown, zombieIpsum on keyup
+const paragraphs = document.querySelectorAll('p');
+paragraphs.forEach(element => {
+    window.addEventListener('keydown', () => {
+        element.textContent = evilLaugh;
+    });
+
+    window.addEventListener('keyup', () => {
+        element.textContent = zombieIpsum;
+    });
+});
+
+// Change div.destination background and text color on click
+const destinations = document.querySelectorAll('.destination');
+destinations.forEach(element => {
+    element.addEventListener('click', () => {
+        element.style.backgroundColor = 'red';
+        element.style.color = 'black';
+    });
+});
+
+// Change .btn background and text color on click, without propagating
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(element => {
+    element.addEventListener('click', (event) => {
+        event.stopPropagation();
+        element.style.backgroundColor = 'black';
+        element.style.color = 'orange';
+    });
+});
+
+// Prevent nav links from refreshing page
+const stopNavLinks = document.querySelectorAll('nav a');
+stopNavLinks.forEach(element => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
     });
 });
