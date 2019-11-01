e01cb4b25749d2a02bb922563df581e37ac1f306
Ben Koehler <bkoehler85282@gmail.com>
2019-10-31 18:04:12 -0700

    added mouse over and scrolling options

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const headerTwo = document.querySelectorAll('h2');
+
+headerTwo[0].addEventListener("mouseover", ele => {
+    ele.target.style.color = 'blue';
+});
+
+const scrollPage = document.querySelector('body');
+
+window.addEventListener('scroll', () => {
+    scrollPage.style.backgroundColor = "lightblue";
+
+    setTimeout(() => {
+        scrollPage.style.backgroundColor = "";
+    }, 3000);
+});
